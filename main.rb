c4430d212cf2f03bee56237a67a89cf22a4fac21
Максим Тимофеев <maxim.timofeev@travelline.lan>
2018-04-06 19:13:19 +0300

    'lesson2'

--- main.rb
+++ main.rb
@@ -1,3 +1,21 @@
 require 'pry'
+require 'tty-cursor'
 
-puts 'Hello world!'
+def draw(row, col, symbol)
+  cursor = TTY::Cursor
+  print cursor.move_to(row, col), symbol
+end
+
+system 'cls'
+
+x1 = 1
+y1 = 3
+char1 = '*'
+draw(x1, y1, char1)
+
+x2 = 4
+y2 = 5
+char2 = '#'
+draw(x2, y2, char2)
+
+gets
