fda68566aac74d6bf7ed8aca02c036160befc27c
maximtimofeev <makscarlos@hotmail.com>
2018-04-16 23:03:33 +0300

    score system

--- main.rb
+++ main.rb
@@ -4,13 +4,33 @@ require_relative 'walls'
 require_relative 'snake'
 require_relative 'food_creator'
 
-print "\e[8;25;80t"
+FIELD_WIDTH = 80
+FIELD_HEIGHT = 25
+SCORE_MESSAGE = 'Total score: '
+
+print "\e[8;27;80t"
 system 'clear'
-system 'cls'
+system('echo -n -e', '\e[?17;14;224c')
+
+def print_score(score)
+  cursor = TTY::Cursor
+  print cursor.move_to(SCORE_MESSAGE.length, FIELD_HEIGHT), score * 100
+end
+
+def game_over
+  cursor = TTY::Cursor
+  center_x = FIELD_WIDTH / 2 - 4
+  center_y = FIELD_HEIGHT / 2
+  print cursor.move_to(center_x, center_y), 'Game over!'
+  print cursor.move_to(0, FIELD_HEIGHT), "Total score:\n"
+  print cursor.move_to(0, FIELD_HEIGHT + 1)
+  exit
+end
 
 # walls
-walls = Walls.new(80, 25)
+walls = Walls.new(FIELD_WIDTH, FIELD_HEIGHT)
 walls.draw
+print TTY::Cursor.move_to(0, FIELD_HEIGHT), SCORE_MESSAGE, 0
 
 # snake
 point = Point.new(4, 5, '*')
@@ -18,7 +38,7 @@ snake = Snake.new(point, 3, Direction::RIGHT)
 snake.draw
 
 # create food
-food_creator = FoodCreator.new(80, 25, '$')
+food_creator = FoodCreator.new(FIELD_WIDTH, FIELD_HEIGHT, '$')
 food = food_creator.create_food
 food.draw(:red)
 
@@ -30,9 +50,10 @@ threads = []
 threads << Thread.new do
   loop do
 
-    exit if snake.hit?(walls) || snake.eat_self?
+    game_over if snake.hit?(walls) || snake.eat_self?
 
     if snake.eat?(food)
+      print_score(snake.points_eated)
       food = food_creator.create_food
       food.draw(:red)
     else
